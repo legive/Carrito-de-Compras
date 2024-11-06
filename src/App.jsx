afd0be43bf310cc41d1451a3b5b892bedbb91ddd
legive <leylagisela@gmail.com>
2024-11-06 14:15:57 -0600

    Empaquetado de aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     const localStorageCart = localStorage.getItem("cart");
     return localStorageCart ? JSON.parse(localStorageCart) : [];
   };
-  const [data, setData] = useState(db);
+  const [data] = useState(db);
 
   const [cart, setCart] = useState(initialCart);
 
